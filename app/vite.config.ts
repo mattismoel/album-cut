ab4e6f8cd55309db224a4746d85c3678949ef00f
Mattis Kristensen <mattismoel@gmail.com>
2026-07-24 15:06:07 +0200

    enable experiemental features for sveltekit

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -8,8 +8,10 @@ export default defineConfig({
       compilerOptions: {
         // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
         runes: ({ filename }) =>
-          filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+          filename.split(/[/\\]/).includes('node_modules') ? undefined : true,
+        experimental: { async: true }
       },
+      experimental: { remoteFunctions: true },
 
       // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
       // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
