8d52f8a49c66d1d2136a1ad27fc26ea632f090a0
Mattis Kristensen <mattismoel@gmail.com>
2026-07-26 16:26:32 +0200

    add class to form field

--- app/src/lib/components/FormField.svelte
+++ app/src/lib/components/FormField.svelte
@@ -2,16 +2,18 @@
 	import type { RemoteFormIssue } from '@sveltejs/kit';
 	import type { Snippet } from 'svelte';
 	import IssueList from './IssueList.svelte';
+	import type { ClassValue } from 'svelte/elements';
 
 	type Props = {
 		issues?: RemoteFormIssue[] | undefined;
 		children: Snippet<[]>;
+		class?: ClassValue;
 	};
 
-	let { issues, children }: Props = $props();
+	let { issues, children, ...rest }: Props = $props();
 </script>
 
-<div class="stack-2">
+<div class={['stack-2', rest.class]}>
 	<div>
 		{@render children()}
 	</div>
