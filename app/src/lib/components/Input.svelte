247e417c04133e5e3ff307a99934f18f146bec62
Mattis Kristensen <mattismoel@gmail.com>
2026-07-26 12:46:49 +0200

    make input element bindable

--- app/src/lib/components/Input.svelte
+++ app/src/lib/components/Input.svelte
@@ -2,10 +2,11 @@
 	import type { HTMLInputAttributes } from 'svelte/elements';
 
 	type Props = HTMLInputAttributes & {
+		element?: HTMLInputElement | undefined;
 		align?: 'left' | 'center' | 'right';
 	};
 
-	let { align = 'left', ...rest }: Props = $props();
+	let { align = 'left', element = $bindable(), ...rest }: Props = $props();
 </script>
 
-<input {...rest} data-align={align} class="input" />
+<input {...rest} data-align={align} class="input" bind:this={element} />
