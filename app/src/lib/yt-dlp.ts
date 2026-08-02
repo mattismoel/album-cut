e7ee32fbacf8de6dec1ed742a07b5b598a0844d6
Mattis Kristensen <mattismoel@gmail.com>
2026-08-02 18:24:57 +0200

    add cookies to youtube request

--- app/src/lib/yt-dlp.ts
+++ app/src/lib/yt-dlp.ts
@@ -1,5 +1,6 @@
 import path from "path"
 import { createCompoundCommand, execAsync, quote } from "./cmd"
+import type { Browser } from "./request"
 
 export type Format = "bestaudio"
 
@@ -9,9 +10,10 @@ export type Format = "bestaudio"
  * @param outDir - The directory to put the downloaded file in.
  * @param format - The format to use.
  */
-export const downloadVideo = async (url: string, outDir: string, fileName: string, format: Format) => {
+export const downloadVideo = async (url: string, outDir: string, fileName: string, format: Format, browser: Browser) => {
   let cmd = createCompoundCommand(
     "yt-dlp",
+    "--cookies-from-browser", browser,
     "-f", format,
     "-o", path.join(outDir, quote(`${fileName}.%(ext)s`)),
     "--print", "after_move:filepath",
