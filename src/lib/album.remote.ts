def16549fab6f0f2b1dfcf22d97dced1b5981be6
Mattis Kristensen <mattismoel@gmail.com>
2026-09-03 19:20:01 +0200

    move to root

--- src/lib/album.remote.ts
+++ src/lib/album.remote.ts
@@ -19,6 +19,7 @@ export const createAlbum = form(albumFormSchema, async ({ url, tracks, ...album 
   const tmpDir = await createTempDir("album-cut")
 
   const cleanUp = (tmpDir: string) => {
+    console.log("cleaning up...")
     fs.rm(tmpDir, { recursive: true })
   }
 
@@ -26,27 +27,40 @@ export const createAlbum = form(albumFormSchema, async ({ url, tracks, ...album 
     const coverExt = path.extname(album.coverArt.name)
     const coverPath = path.join(tmpDir, `cover${coverExt}`)
 
+    console.log("saving cover art...")
     await saveFile(album.coverArt, coverPath)
+    console.log("saved cover art...")
 
     const fileName = `${formatArtistsNames(album.albumArtists)} - ${album.title} (${album.releaseDate})`
 
+    console.log("downloading video...")
     const videoOutPath = await downloadVideo(url, tmpDir, fileName, "bestaudio", browser)
+    console.log("downloaded video...")
+    console.log("converting file...")
     const audioOutPath = await convertFile(videoOutPath, "mp3")
+    console.log("converted file...")
 
+    console.log("cutting tracks...")
     const trackPaths = await cutTracks(audioOutPath, tracks, {
       ...album,
       album: album.title,
       coverArt: coverPath,
     })
+    console.log("cut tracks...")
 
     const zipOutPath = path.join(tmpDir, `${fileName}.zip`)
 
+    console.log("zipping files...")
     await zipFiles(zipOutPath, [...trackPaths, coverPath], { deleteOriginal: true })
+    console.log("zipped files...")
 
+    console.log("creating download...")
     const downloadId = createDownload(zipOutPath)
 
+    console.log("redirecting...")
     redirect(303, `/download?id=${downloadId}`)
   } catch (err) {
+    console.error(err)
     if (isRedirect(err)) throw err
 
     cleanUp(tmpDir)
